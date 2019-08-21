16c5ec5bea6f29c42d3e7a7fc767c34dc40d2d88
Yamahari <40495288+Yamahari@users.noreply.github.com>
2019-08-21 19:47:44 +0200

    tags + working scaffoldings

--- src/main/java/yamahari/ilikewood/tags/WoodenBlockTags.java
+++ src/main/java/yamahari/ilikewood/tags/WoodenBlockTags.java
@@ -1,4 +1,19 @@
 package yamahari.ilikewood.tags;
 
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+import yamahari.ilikewood.util.Constants;
+
+@SuppressWarnings("unused")
 public class WoodenBlockTags {
+    public static final Tag<Block> BARRELS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "barrels"));
+    public static final Tag<Block> BOOKSHELFS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "bookshelfs"));
+    public static final Tag<Block> CHESTS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "chests"));
+    public static final Tag<Block> COMPOSTERS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "composters"));
+    public static final Tag<Block> LADDERS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "ladders"));
+    public static final Tag<Block> PANELS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "panels"));
+    public static final Tag<Block> SCAFFOLDINGS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "scaffoldings"));
+    public static final Tag<Block> WALLS = new BlockTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "walls"));
 }
--- src/main/java/yamahari/ilikewood/tags/WoodenItemTags.java
+++ src/main/java/yamahari/ilikewood/tags/WoodenItemTags.java
@@ -6,6 +6,15 @@ import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
 import yamahari.ilikewood.util.Constants;
 
+@SuppressWarnings("unused")
 public class WoodenItemTags {
-    public static final Tag<Item> SCAFFOLDINGS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "tags/items/scaffoldings"));
+    public static final Tag<Item> BARRELS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "barrels"));
+    public static final Tag<Item> BOOKSHELFS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "bookshelfs"));
+    public static final Tag<Item> CHESTS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "chests"));
+    public static final Tag<Item> COMPOSTERS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "composters"));
+    public static final Tag<Item> LADDERS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "ladders"));
+    public static final Tag<Item> PANELS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "panels"));
+    public static final Tag<Item> SCAFFOLDINGS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "scaffoldings"));
+    public static final Tag<Item> STICKS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "sticks"));
+    public static final Tag<Item> WALLS = new ItemTags.Wrapper(new ResourceLocation(Constants.MOD_ID, "walls"));
 }
